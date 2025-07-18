06b3d11a019ad6d5823a3c503176c42a7df1fa2a
Luckylikithh <kodumurilikith4@gmail.com>
2025-07-18 13:45:53 -0400

    port changes

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Currency Exchange API", version="2.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], #connecting frontend to backend.
+    allow_origins=["https://lustrous-pie-54fe17.netlify.app"], #connecting frontend to backend.
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
