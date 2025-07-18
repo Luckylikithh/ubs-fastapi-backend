34aa1eb176ec0861cedb3bccce593f924b3ec0d1
Luckylikithh <kodumurilikith4@gmail.com>
2025-07-18 12:43:53 -0400

    new changes

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://conversion_db_user:cfT1YV89IKhP1UJyAXIzyDI6oxoMV8x0@dpg-d1t74v3uibrs738vu1tg-a.oregon-postgres.render.com/conversion_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
